b29645c81908937b5d06108dcbde4c64ef314d87
Anderson Rodrigues <anddersonrds@gmail.com>
2020-11-18 17:59:03 -0300

    add MovieDetail page

---
+++ src/pages/MovieDetail/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+import { Container } from './styles';
+
+const MovieDetail = () => {
+  return (
+    <Container>
+      <Header back />
+    </Container>
+  );
+};
+
+export default MovieDetail;
