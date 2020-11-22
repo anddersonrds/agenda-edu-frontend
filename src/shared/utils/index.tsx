bea0d45143c98a64a24b7ae1d61d1a474a2d1499
Anderson Rodrigues <anddersonrds@gmail.com>
2020-11-22 00:14:53 -0300

    add easter-egg

--- src/shared/utils/index.tsx
+++ src/shared/utils/index.tsx
@@ -1,3 +1,5 @@
+import easteregg from '../assets/easter-egg.jpg';
+
 export const getJustYear = (date: string) => {
   return date.substr(0, 4);
 };
@@ -15,10 +17,23 @@ export const getLocaleDate = (date: string) => {
   return formattedDate;
 };
 
-export const getSmallImage = (link: string) => {
-  return `https://image.tmdb.org/t/p/w342/${link}`;
+export const getMediumImage = (link: string) => {
+  if (link !== null) {
+    return `https://image.tmdb.org/t/p/w342/${link}`;
+  }
+  return easteregg;
 };
 
-export const getBigImage = (link: string) => {
-  return `https://image.tmdb.org/t/p/w500/${link}`;
+export const getLargeImage = (link: string) => {
+  if (link !== null) {
+    return `https://image.tmdb.org/t/p/w500/${link}`;
+  }
+  return easteregg;
+};
+
+export const getSmallImage = (link: string) => {
+  if (link !== null) {
+    return `https://image.tmdb.org/t/p/w154${link}`;
+  }
+  return easteregg;
 };
