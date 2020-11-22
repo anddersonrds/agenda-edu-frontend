c8806cf801c2b514f8d678ed241acfbced82aaa1
Anderson Rodrigues <anddersonrds@gmail.com>
2020-11-22 00:13:59 -0300

    remove search page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../modules/pages/Home';
-import Search from '../modules/pages/Search';
 import MovieDetail from '../modules/pages/MovieDetail';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/movies/:id" component={MovieDetail} />
-    <Route path="/movies/search" component={Search} />
   </Switch>
 );
 
